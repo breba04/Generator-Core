819d4ee95dd5c759dcb770c1418f9a8303fe1223
Breba Hamid <ibrahimabdo407@gmail.com>
2026-01-31 16:17:06 +0200

    Add project files.

---
+++ clsColumnSchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_Core
+{
+    public class clsColumnSchema
+    {
+        public int ColumnID { get; set; }
+        public string ColumnName { get; set; }
+        public string ColumnType { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public bool NotNull { get; set; }
+    }
+}
---
+++ clsSqlGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_Core
+{
+    static public class clsSqlGenerator
+    {
+        static public string GenerateCreateDatabaseScript(string databaseName)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Breba')");
+            sb.AppendLine($"CREATE DATABASE Breba;");
+            return sb.ToString();
+        }
+        static public string GenerateCreateTableScript(string databaseName)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Breba')");
+            sb.AppendLine($"CREATE DATABASE Breba;");
+            return sb.ToString();
+        }
+    }
+}
