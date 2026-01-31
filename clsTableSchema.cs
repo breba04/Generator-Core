54d688f9ed4896d6928c5a760f80c132b3a613e5
Breba Hamid <ibrahimabdo407@gmail.com>
2026-01-31 17:20:12 +0200

    add class 'table schema'

---
+++ clsTableSchema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_Core
+{
+    public class clsTableSchema
+    {
+        string TableName { get; set; }
+        List<clsColumnSchema> Columns { get; set; }
+        public clsTableSchema(string TableName, List<clsColumnSchema> Columns)
+        {
+            this.TableName = TableName;
+            this.Columns = Columns;
+        }
+    }
+}
