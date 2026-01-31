798ce7889101ce4e2ec8c60920520c354b4e47b6
Breba Hamid <ibrahimabdo407@gmail.com>
2026-01-31 18:06:15 +0200

    add 'generator table' function

--- clsSqlGenerator.cs
+++ clsSqlGenerator.cs
@@ -11,15 +11,29 @@ namespace Generator_Core
         static public string GenerateCreateDatabaseScript(string databaseName)
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Breba')");
-            sb.AppendLine($"CREATE DATABASE Breba;");
+            sb.AppendLine($"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = ' {databaseName} ')");
+            sb.AppendLine($"CREATE DATABASE  {databaseName};");
             return sb.ToString();
         }
-        static public string GenerateCreateTableScript(string databaseName)
+        static public string GenerateCreateTableScript(string databaseName , clsTableSchema Table)
         {
+            bool isAddedPrimaryKey = false;
+            string isNullable = "";
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Breba')");
-            sb.AppendLine($"CREATE DATABASE Breba;");
+            sb.AppendLine($"USE  {databaseName}");
+            sb.AppendLine($"CREATE TABLE [{Table.TableName}] ( ");
+            foreach (clsColumnSchema column in Table.Columns)
+            {
+                isNullable = column.NotNull ? "NOT NULL" : "NULL";
+                sb.Append($"  [ {column.ColumnName}] {column.ColumnType} {isNullable} ");
+                if (!isAddedPrimaryKey && column.IsPrimaryKey)
+                {
+                    sb.Append(" PRIMARY KEY");
+                    isAddedPrimaryKey = true;
+                }
+                sb.Append(',');
+            }
+            sb.Replace(",", ") ",sb.Length - 1,1);
             return sb.ToString();
         }
     }
