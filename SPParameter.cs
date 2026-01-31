3f98079da6a01d5816c2cc96d2f5892a2c9235f1
Breba Hamid <ibrahimabdo407@gmail.com>
2026-01-31 18:18:34 +0200

    add class for parameters of stored procedure

---
+++ SPParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_Core
+{
+    public class SPParameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool isOutput { get; set; }
+        public SPParameter(string Name, string Type, bool isOutput)
+        {
+            this.Name = Name;
+            this.Type = Type;
+            this.isOutput = isOutput;
+        }
+    }
+}
