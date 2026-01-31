29f2fd25498c01051a4b0a9baba3a0647703ebc0
Breba Hamid <ibrahimabdo407@gmail.com>
2026-01-31 18:39:46 +0200

    ADD Procedure Schema

---
+++ clsProcedureSchema.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_Core
+{
+    public class clsProcedureSchema
+    {
+        public enum enProcedureType
+        {
+            Select,
+            Insert,
+            Update,
+            Delete,
+            Other
+        }
+        public string ProcedureName { get; set; }
+        public List<clsSPParameter> Parameters { get; set; }
+        public enProcedureType SPType { get; set; } 
+        public clsProcedureSchema(string ProcedureName, List<clsSPParameter> Parameters, enProcedureType SPType)
+        {
+            this.ProcedureName = ProcedureName;
+            this.Parameters = Parameters;
+            this.SPType = SPType;
+        }
+    }
+}
